6f00e4c58f8f841769a4e89bcceb699e2fbd195c
amuse1991 <amuse1991@gmail.com>
2021-12-13 19:23:01 +0900

    글로벌 스타일 적용

---
+++ styles/palette.ts
@@ -0,0 +1,12 @@
+export const palette = {
+  black: "#2222222",
+  black_denim: "#252934",
+  white_snow: "#FFF8F6",
+  pink: "#DF1B6B",
+  gray: "#45474A",
+  blue_azure: "#0080FE",
+  blue_cornflower: "#6693F5",
+  charcoal: "#484848"
+};
+
+export default palette;
