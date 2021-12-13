132031941fc9d2cb888c96495da2cc2fe136d9cd
amuse1991 <amuse1991@gmail.com>
2021-12-13 19:05:19 +0900

    styled-components SSR설정

---
+++ pages/_document.tsx
@@ -0,0 +1,33 @@
+import Document, { DocumentContext } from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+export default class MyDocument extends Document {
+  static async getInitProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () => {
+        return originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+        });
+      };
+
+      const initalProps = await Document.getInitialProps(ctx);
+
+      return {
+        ...initalProps,
+        styles: (
+          <>
+            {initalProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      };
+    } catch (err) {
+      console.error(err);
+    } finally {
+      sheet.seal();
+    }
+  }
+}
