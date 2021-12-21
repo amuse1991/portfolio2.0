0ee06a9aabe8bba90d52a1a4a04e1f664a9a78f6
amuse1991 <amuse1991@gmail.com>
2021-12-21 19:00:15 +0900

    헤더 모바일 화면 작업

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
 import Header from "@components/Header";
+import Intro from "@components/Intro";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import styled from "styled-components";
 import styles from "../styles/Home.module.css";
 
+const Container = styled.div``;
 const Home: NextPage = () => {
-  return <Header />;
+  return (
+    <Container>
+      <Header />
+      <Intro />
+    </Container>
+  );
 };
 
 export default Home;
