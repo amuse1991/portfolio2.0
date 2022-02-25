377c1099c32627797b0917392881d33300bd1953
amuse1991 <amuse1991@gmail.com>
2022-02-26 00:30:04 +0900

    스크롤 복원 기능 성능개선

--- hooks/useScroll.ts
+++ hooks/useScroll.ts
@@ -3,35 +3,29 @@ import { useRouter } from "next/router";
 import * as _ from "lodash";
 
 const useScroll = () => {
-  const [scrollY, setScrollY] = useState(0);
   const router = useRouter();
+  const saveScroll = () => {
+    window.sessionStorage.setItem("scrollY", String(window.scrollY));
+  };
+  const loadScroll = () => {
+    const scrollY = window.sessionStorage.getItem("scrollY");
+    if (scrollY) {
+      window.scrollBy({ top: Number(scrollY) });
+      window.sessionStorage.setItem("scrollY", String(0));
+    }
+  };
 
   useEffect(() => {
-    const handleScroll = _.throttle(() => {
-      setScrollY(window.scrollY);
-    }, 1000);
-    const loadScroll = () => {
-      const y = window.sessionStorage.getItem("scrollY");
-      if (y) {
-        window.scrollBy({ top: Number(y) });
-        window.sessionStorage.setItem("scrollY", String(0));
-      }
-    };
-    handleScroll();
-    loadScroll();
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
-  useEffect(() => {
-    const saveScroll = url => {
-      window.sessionStorage.setItem("scrollY", String(scrollY));
-    };
     router.events.on("routeChangeStart", saveScroll);
     return () => {
       router.events.off("routeChangeStart", saveScroll);
     };
-  }, [router.events, scrollY]);
+    // eslint-disable-next-line
+  }, []);
+
+  useEffect(() => {
+    loadScroll();
+  }, [router.events]);
 };
 
 export default useScroll;
